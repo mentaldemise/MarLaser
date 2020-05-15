58a7ccd14d876964db29d82226b42aa08cd8227c
Don Goss <don.goss@gmail.com>
2020-05-15 03:35:19 -0400

    Latest MKS setting tweaks. No idea what I'm doing. Trying to slam the head back and forth faster with the 2A stepper.

--- LaserMarlin-bugfix-2.0.x-MKS/Marlin/Configuration.h
+++ LaserMarlin-bugfix-2.0.x-MKS/Marlin/Configuration.h
@@ -777,17 +777,17 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-//#define CLASSIC_JERK
+#define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
-  #define DEFAULT_XJERK 10.0
-  #define DEFAULT_YJERK 10.0
+  #define DEFAULT_XJERK 20.0
+  #define DEFAULT_YJERK 20.0
   #define DEFAULT_ZJERK  0.3
 
   //#define TRAVEL_EXTRA_XYJERK 0.0     // Additional jerk allowance for all travel moves
 
-  //#define LIMITED_JERK_EDITING        // Limit edit via M205 or LCD to DEFAULT_aJERK * 2
+  #define LIMITED_JERK_EDITING        // Limit edit via M205 or LCD to DEFAULT_aJERK * 2
   #if ENABLED(LIMITED_JERK_EDITING)
-    #define MAX_JERK_EDIT_VALUES { 20, 20, 0.6, 10 } // ...or, set your own edit limits
+    #define MAX_JERK_EDIT_VALUES { 100, 100, 100, 10 } // ...or, set your own edit limits
   #endif
 #endif
 
