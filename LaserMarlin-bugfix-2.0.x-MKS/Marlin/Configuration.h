6640636912756756209f2c6c821e81196adecfeb
Don Goss <don.goss@gmail.com>
2020-05-21 21:39:40 -0400

    Setting changes for swapped in Y motor(1.8A with dual shaft)

--- LaserMarlin-bugfix-2.0.x-MKS/Marlin/Configuration.h
+++ LaserMarlin-bugfix-2.0.x-MKS/Marlin/Configuration.h
@@ -730,7 +730,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.25, 158.5, 4000, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.25, 79.25, 4000, 500 }
 
 /**
  * Default Max Feed Rate (mm/s)
--- LaserMarlin-bugfix-2.0.x-MKS/Marlin/Configuration_adv.h
+++ LaserMarlin-bugfix-2.0.x-MKS/Marlin/Configuration_adv.h
@@ -2124,7 +2124,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       900
+    #define Y_CURRENT       1300
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
